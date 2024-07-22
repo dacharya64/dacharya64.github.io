8a92bad73b719f883d9ad077007818d6b280050b
Devi <dacharya64@proton.me>
2024-07-22 13:05:23 -0400

    header and dark mode toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,13 @@
 	import Publications from './publications.svelte';
 	import Projects from './projects.svelte';
 	import Bio from './bio.svelte';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
 </script>
+
+<header class="flex p-4 justify-between">
+	<div><h4 class="h4"><strong>Devi Acharya</strong> | AI in Games</h4></div>
+	<div><LightSwitch /></div>
+</header>
 
 <section class="homepage">
 	<Bio />
