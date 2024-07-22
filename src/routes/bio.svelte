65a382dc55999944e8bfef2d233fd93c5c079d91
Devi <dacharya64@proton.me>
2024-07-22 17:08:05 -0400

    formatting for bio

--- src/routes/bio.svelte
+++ src/routes/bio.svelte
@@ -1,9 +1,16 @@
-<div class="flex p-4 justify-between">
-	<span>
+<div class="grid grid-rows-1 grid-cols-8 gap-4">
+	<div class="md:col-start-2 col-end-3">
+		<img class="rounded-full" src="/img/devi.jpg" alt="" />
+	</div>
+	<div class="md:col-span-5">
+		<h2 class="h2">
+			<i class="fa-solid fa-gears"></i>
+			<span class="intro">Hi, I'm <strong>Devi Acharya</strong></span>
+		</h2>
 		<p>
-			I'm Devi Acharya, a researcher and developer working at the intersection of AI and interactive storytelling. I am currently part of Zynga's Applied AI team, where I work on tools, games, and
-			demos focused on creative applications of AI. I received my Master's degree in Computational Media from the University of California, Santa Cruz, where I developed AI tools for game masters of
-			tabletop roleplaying games.
+			I'm a researcher and developer working at the intersection of AI and interactive storytelling. I am currently part of Zynga's Applied AI team, where I work on tools, games, and demos focused on
+			creative applications of AI. I received my Master's degree in Computational Media from the University of California, Santa Cruz, where I developed AI tools for game masters of tabletop
+			roleplaying games.
 		</p>
-	</span>
+	</div>
 </div>
