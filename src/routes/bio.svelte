e9961576c1584ad0e6a70925278df7e148749d6c
Devi <dacharya64@proton.me>
2024-07-23 14:22:02 -0400

    refactor timeline

--- src/routes/bio.svelte
+++ src/routes/bio.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Timeline from './timeline.svelte';
+</script>
+
 <div class="grid grid-rows-1 gap-4 grid-cols-[repeat(auto-fit,_14.666666%)] m-auto justify-center">
 	<div class="col-span-1 justify-center justify-self-center mx-auto">
 		<img class="rounded-full" src="/img/devi.jpg" alt="" />
@@ -10,32 +14,6 @@
 			</h2>
 			<h4 class="h4">I design and develop AI tools for games & interactive media</h4>
 		</div>
-		<div>
-			<ol class="relative border-s border-primary-700 dark:border-primary-200">
-				<li class="mb-10 ms-4">
-					<div class="absolute w-3 h-3 bg-primary-700 rounded-full mt-1.5 -start-1.5 border border-primary-700 dark:border-primary-200 dark:bg-primary-200"></div>
-					<time class="mb-1">2022 - Now</time>
-					<h4 class="h4"><strong>Senior Data Scientist</strong>, Applied AI @ Zynga</h4>
-					<p>Developed AI-based tools to support game and creative teams at Zynga</p>
-					<span><span class="badge variant-filled">Procedural Content Generation</span><span class="badge variant-filled">Storylets</span><span class="badge variant-filled">Generative AI</span></span>
-				</li>
-				<li class="mb-10 ms-4">
-					<div class="absolute w-3 h-3 bg-primary-700 rounded-full mt-1.5 -start-1.5 border border-primary-700 dark:border-primary-200 dark:bg-primary-200"></div>
-					<time class="mb-1">2018 - 2022</time>
-					<h4 class="h4"><strong>Graduate Research Assistant</strong>, Expressive Intelligence Studio @ University of California, Santa Cruz</h4>
-					<p><strong>MS in Computational Media</strong></p>
-					<p>Designed and developed AI support tools for game masters of tabletop roleplaying games</p>
-					<span><span class="badge variant-filled">Story Sifting</span><span class="badge variant-filled">Prolog</span><span class="badge variant-filled">Social Simulation</span></span>
-				</li>
-				<li class="mb-10 ms-4">
-					<div class="absolute w-3 h-3 bg-primary-700 rounded-full mt-1.5 -start-1.5 border border-primary-700 dark:border-primary-200 dark:bg-primary-200"></div>
-					<time class="mb-1">2014-2018</time>
-					<h4 class="h4"><strong>Game Designer & Developer</strong> @ Brandeis University</h4>
-					<p><strong>BA in Computer Science; BA in Film, TV, and Interactive Media</strong></p>
-					<p>Designed and developed indie games and interactive media projects</p>
-					<span><span class="badge variant-filled">Unity</span><span class="badge variant-filled">Java</span></span>
-				</li>
-			</ol>
-		</div>
+		<Timeline />
 	</div>
 </div>
---
+++ src/routes/timeline.svelte
@@ -0,0 +1,27 @@
+<div>
+	<ol class="relative border-s border-primary-700 dark:border-primary-200">
+		<li class="mb-10 ms-4">
+			<div class="absolute w-3 h-3 bg-primary-700 rounded-full mt-1.5 -start-1.5 border border-primary-700 dark:border-primary-200 dark:bg-primary-200"></div>
+			<time class="mb-1">2022 - Now</time>
+			<h4 class="h4"><strong>Senior Data Scientist</strong>, Applied AI @ Zynga</h4>
+			<p>Developed AI-based tools to support game and creative teams at Zynga</p>
+			<span><span class="badge variant-filled">Procedural Content Generation</span><span class="badge variant-filled">Storylets</span><span class="badge variant-filled">Generative AI</span></span>
+		</li>
+		<li class="mb-10 ms-4">
+			<div class="absolute w-3 h-3 bg-primary-700 rounded-full mt-1.5 -start-1.5 border border-primary-700 dark:border-primary-200 dark:bg-primary-200"></div>
+			<time class="mb-1">2018 - 2022</time>
+			<h4 class="h4"><strong>Graduate Research Assistant</strong>, Expressive Intelligence Studio @ University of California, Santa Cruz</h4>
+			<p><strong>MS in Computational Media</strong></p>
+			<p>Designed and developed AI support tools for game masters of tabletop roleplaying games</p>
+			<span><span class="badge variant-filled">Story Sifting</span><span class="badge variant-filled">Prolog</span><span class="badge variant-filled">Social Simulation</span></span>
+		</li>
+		<li class="mb-10 ms-4">
+			<div class="absolute w-3 h-3 bg-primary-700 rounded-full mt-1.5 -start-1.5 border border-primary-700 dark:border-primary-200 dark:bg-primary-200"></div>
+			<time class="mb-1">2014-2018</time>
+			<h4 class="h4"><strong>Game Designer & Developer</strong> @ Brandeis University</h4>
+			<p><strong>BA in Computer Science; BA in Film, TV, and Interactive Media</strong></p>
+			<p>Designed and developed indie games and interactive media projects</p>
+			<span><span class="badge variant-filled">Unity</span><span class="badge variant-filled">Java</span></span>
+		</li>
+	</ol>
+</div>
