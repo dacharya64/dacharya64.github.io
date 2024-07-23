0a8906e926bce30c3cba0185600ced5ab746fa84
Devi <dacharya64@proton.me>
2024-07-23 13:57:30 -0400

    adding badges

--- src/routes/bio.svelte
+++ src/routes/bio.svelte
@@ -1,4 +1,4 @@
-<div class="grid grid-rows-1 gap-4 grid-cols-[repeat(auto-fit,_10.666666%)] m-auto justify-center">
+<div class="grid grid-rows-1 gap-4 grid-cols-[repeat(auto-fit,_14.666666%)] m-auto justify-center">
 	<div class="col-span-1 justify-center justify-self-center mx-auto">
 		<img class="rounded-full" src="/img/devi.jpg" alt="" />
 	</div>
@@ -16,30 +16,26 @@
 					<div class="absolute w-3 h-3 bg-primary-700 rounded-full mt-1.5 -start-1.5 border border-primary-700 dark:border-primary-200 dark:bg-primary-200"></div>
 					<time class="mb-1">2022 - Now</time>
 					<h4 class="h4"><strong>Senior Data Scientist</strong>, Applied AI @ Zynga</h4>
-					<p>Developing AI-based tools to support game and creative teams at Zynga</p>
+					<p>Developed AI-based tools to support game and creative teams at Zynga</p>
+					<span><span class="badge variant-filled">Procedural Content Generation</span><span class="badge variant-filled">Storylets</span><span class="badge variant-filled">Generative AI</span></span>
 				</li>
 				<li class="mb-10 ms-4">
 					<div class="absolute w-3 h-3 bg-primary-700 rounded-full mt-1.5 -start-1.5 border border-primary-700 dark:border-primary-200 dark:bg-primary-200"></div>
 					<time class="mb-1">2018 - 2022</time>
 					<h4 class="h4"><strong>Graduate Research Assistant</strong>, Expressive Intelligence Studio @ University of California, Santa Cruz</h4>
-					<p>Designed and developed AI support tools for game masters of tabletop roleplaying games</p>
 					<p><strong>MS in Computational Media</strong></p>
+					<p>Designed and developed AI support tools for game masters of tabletop roleplaying games</p>
+					<span><span class="badge variant-filled">Story Sifting</span><span class="badge variant-filled">Prolog</span><span class="badge variant-filled">Social Simulation</span></span>
 				</li>
 				<li class="mb-10 ms-4">
 					<div class="absolute w-3 h-3 bg-primary-700 rounded-full mt-1.5 -start-1.5 border border-primary-700 dark:border-primary-200 dark:bg-primary-200"></div>
 					<time class="mb-1">2014-2018</time>
 					<h4 class="h4"><strong>Game Designer & Developer</strong> @ Brandeis University</h4>
+					<p><strong>BA in Computer Science; BA in Film, TV, and Interactive Media</strong></p>
 					<p>Designed and developed indie games and interactive media projects</p>
-					<p><strong>BA in Computer Science</strong></p>
-					<p><strong>BA in Film, TV, and Interactive Media</strong></p>
+					<span><span class="badge variant-filled">Unity</span><span class="badge variant-filled">Java</span></span>
 				</li>
 			</ol>
 		</div>
-		<!-- <p>
-             
-			I'm a researcher and developer working at the intersection of AI and interactive storytelling. I am currently part of Zynga's Applied AI team, where I work on tools, games, and demos focused on
-			creative applications of AI. I received my Master's degree in Computational Media from the University of California, Santa Cruz, where I developed AI tools for game masters of tabletop
-			roleplaying games.
-		</p> -->
 	</div>
 </div>
