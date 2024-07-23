3de82b52c74d65309b0d56a6d27343b1fa9ac497
Devi <dacharya64@proton.me>
2024-07-23 13:11:48 -0400

    finishing work on timeline

--- src/routes/bio.svelte
+++ src/routes/bio.svelte
@@ -3,17 +3,35 @@
 		<img class="rounded-full" src="/img/devi.jpg" alt="" />
 	</div>
 	<div class="col-span-5 justify-center justify-self-center mx-auto">
-		<h2 class="h2">
-			<i class="fa-solid fa-gears"></i>
-			<span class="intro">Hi, I'm <strong>Devi Acharya</strong></span>
-		</h2>
+		<div class="bio">
+			<h2 class="h2">
+				<i class="fa-solid fa-gears"></i>
+				<span class="intro">Hi, I'm <strong>Devi Acharya</strong></span>
+			</h2>
+			<h4 class="h4">I design and develop AI tools for games & interactive media</h4>
+		</div>
 		<div>
 			<ol class="relative border-s border-primary-700 dark:border-primary-200">
 				<li class="mb-10 ms-4">
 					<div class="absolute w-3 h-3 bg-primary-700 rounded-full mt-1.5 -start-1.5 border border-primary-700 dark:border-primary-200 dark:bg-primary-200"></div>
-					<time class="mb-1">January 2023 - Present</time>
-					<h4 class="h4">Senior Data Scientist, Applied AI @ Zynga</h4>
+					<time class="mb-1">2022 - Now</time>
+					<h4 class="h4"><strong>Senior Data Scientist</strong>, Applied AI @ Zynga</h4>
 					<p>Developing AI-based tools to support game and creative teams at Zynga</p>
+				</li>
+				<li class="mb-10 ms-4">
+					<div class="absolute w-3 h-3 bg-primary-700 rounded-full mt-1.5 -start-1.5 border border-primary-700 dark:border-primary-200 dark:bg-primary-200"></div>
+					<time class="mb-1">2018 - 2022</time>
+					<h4 class="h4"><strong>Graduate Research Assistant</strong>, Expressive Intelligence Studio @ University of California, Santa Cruz</h4>
+					<p>Designed and developed AI support tools for game masters of tabletop roleplaying games</p>
+					<p><strong>MS in Computational Media</strong></p>
+				</li>
+				<li class="mb-10 ms-4">
+					<div class="absolute w-3 h-3 bg-primary-700 rounded-full mt-1.5 -start-1.5 border border-primary-700 dark:border-primary-200 dark:bg-primary-200"></div>
+					<time class="mb-1">2014-2018</time>
+					<h4 class="h4"><strong>Game Designer & Developer</strong> @ Brandeis University</h4>
+					<p>Designed and developed indie games and interactive media projects</p>
+					<p><strong>BA in Computer Science</strong></p>
+					<p><strong>BA in Film, TV, and Interactive Media</strong></p>
 				</li>
 			</ol>
 		</div>
