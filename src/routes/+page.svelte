12b394e55bb5363db967dfa7536b7ee321d8df72
Devi <dacharya64@proton.me>
2024-07-23 15:57:53 -0400

    adding contact me button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import { LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
+<!-- Header -->
 <header class="flex p-4 justify-between">
 	<div>
 		<h4 class="h4">
@@ -12,11 +13,20 @@
 			| <i class="fa-solid fa-gears"></i> AI in Games
 		</h4>
 	</div>
-	<div><LightSwitch /></div>
+
+	<div>
+		<LightSwitch />
+	</div>
 </header>
 
+<!-- Main body -->
 <section class="homepage">
 	<Bio />
 	<Projects />
 	<Publications />
 </section>
+
+<!-- Contact me button -->
+<div class="fixed bottom-0 w-full">
+	<a href="mailto:dacharya64@proton.me" target="blank" type="button" class="btn-icon btn-icon-xl variant-filled float-right contact"><i class="fa-solid fa-envelope"></i></a>
+</div>
