b75e3e6a27564f791bb12de331ba237ed685ccdf
Devi <dacharya64@proton.me>
2024-08-01 13:16:42 -0400

    adding fusion galaxy page

---
+++ src/routes/fusion/+page.svelte
@@ -0,0 +1,48 @@
+<section class="projectpage">
+	<a href="../" type="button" class="btn variant-filled cardbutton">Back</a>
+
+	<div>
+		<h1 class="h1">Fusion Galaxy</h1>
+		<h5 class="h5">A mobile game about collecting and crafting elements to make life.</h5>
+	</div>
+
+	<p>
+		I worked on <em>Fusion Galaxy</em> with a team of seven, from ideation to release on the App Store and Google Play. <em>Fusion Galaxy</em> is a mobile game where players collect different elements
+		as they fall down the screen, swiping them into matching buckets to collect them. Players can then use the elements that they've collected, crafting them together to discover new elements and even
+		find the formula to create life itself!
+	</p>
+
+	<p>
+		<em>Fusion Galaxy</em> was created as part of the Massachusetts Digital Games Institute's
+		<a href="https://www.massdigi.org/programs-services/summer-innovation-program/" class="anchor"> Summer Innovation Program</a>, a program designed to help young game developers develop games and
+		learn more about the games industry. I worked as the lead artist on the team, helping create an art bible and assets for the team as well as guiding the team's artists.
+	</p>
+
+	<!-- <h2 class="h2">Gallery</h2> -->
+
+	<center>
+		<iframe
+			width="560"
+			height="315"
+			src="https://www.youtube.com/embed/umY061u91Dw?si=Z1c56cEO3ZSs9EF1"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			referrerpolicy="strict-origin-when-cross-origin"
+			allowfullscreen></iframe
+		></center>
+	<section class="grid grid-cols-4 gap-8">
+		<div>
+			<img class="rounded-lg" src="img/fusion1.jpg" alt="" />
+		</div>
+		<div>
+			<img class="rounded-lg" src="img/fusion2.jpg" alt="" />
+		</div>
+		<div>
+			<img class="rounded-lg" src="img/fusion3.jpg" alt="" />
+		</div>
+		<div>
+			<img class="rounded-lg" src="img/fusion4.jpg" alt="" />
+		</div>
+	</section>
+</section>
--- src/routes/warmer/+page.svelte
+++ src/routes/warmer/+page.svelte
@@ -33,9 +33,12 @@
 			The game begins with the player picking a name for their planet, and then seeing the overworld of the game, displaying the different regions of the planet and stats about their world such as
 			temperature, emissions, and popular support.
 		</p>
-		<img
-			src="https://camo.githubusercontent.com/f950fb6212401725dc71057f3f90945aa9f455cd5bde51139de3aea739bcbc2c/68747470733a2f2f692e696d6775722e636f6d2f31665067326b722e706e67"
-			alt="Overworld of game Warmer" />
+		<figure>
+			<img
+				src="https://camo.githubusercontent.com/f950fb6212401725dc71057f3f90945aa9f455cd5bde51139de3aea739bcbc2c/68747470733a2f2f692e696d6775722e636f6d2f31665067326b722e706e67"
+				alt="Overworld of game Warmer" />
+		</figure>
+
 		<p>
 			There are four different minigames available to play, one in each region. The difficulty and contents of these games vary based on global temperature, emissions, and precipitation. The player's
 			choices and performance in these minigames change the world's stats as well, resulting in a loop where the player can see the impacts of their actions on their planet.
@@ -46,9 +49,12 @@
 			somewhat abstracted--such as a large increase in emissions, or a small decrease in popular support). They must choose one of each pair to enact. These bills directly impact the stats of the
 			planet, and players must balance the importance of different factors such as emissions and popular support, as these will impact global temperature and the other minigames.
 		</p>
-		<img
-			src="https://camo.githubusercontent.com/cc6d5e03ae678d762aaf3dbc5052f3c6ba9d4a1dd480e555372d674aa43b190d/68747470733a2f2f692e696d6775722e636f6d2f7a5638514874712e706e67"
-			alt="The city minigame" />
+		<figure>
+			<img
+				src="https://camo.githubusercontent.com/cc6d5e03ae678d762aaf3dbc5052f3c6ba9d4a1dd480e555372d674aa43b190d/68747470733a2f2f692e696d6775722e636f6d2f7a5638514874712e706e67"
+				alt="The city minigame" />
+		</figure>
+
 		<h3 class="h3">Protest Emissions</h3>
 		<p>
 			Another minigame lets the player send people to different areas to help mitigate the effects of climate change. The number of people that the player gets to place is dependent on their current
@@ -59,24 +65,30 @@
 			<li><strong>The forest</strong> -- people placed here will help to mitigate deforestation of the area by planting trees, which will help to somewhat reduce emissions</li>
 			<li><strong>The carbon sequestration plant</strong> -- people placed here will reduce emissions more, but this costs more money overall</li>
 		</ul>
-		<img
-			src="https://camo.githubusercontent.com/cba8f28080cdec274c7c717da2cd387224cf31f991ee96deb0e0797bc1497ca0/68747470733a2f2f692e696d6775722e636f6d2f6f684c494344452e706e67"
-			alt="The protest minigame" />
+		<figure>
+			<img
+				src="https://camo.githubusercontent.com/cba8f28080cdec274c7c717da2cd387224cf31f991ee96deb0e0797bc1497ca0/68747470733a2f2f692e696d6775722e636f6d2f6f684c494344452e706e67"
+				alt="The protest minigame" />
+		</figure>
 		<h3 class="h3">Fight Wildfires</h3>
 		<p>
 			The third minigame allows the player to react to the effects of climate change. As the world becomes hotter and drier, wildfires spread. This minigame allows the player to fly a helicopter,
 			picking up water from reservoirs and using this water to put out fires in the area. The size of reservoirs and fires is dependent on global stats, making the game harder as the planet heats up.
 		</p>
-		<img
-			src="https://camo.githubusercontent.com/999bed0c77f49d2145630fa9476bc91d18912ce0affccb745be612d6681e0ee0/68747470733a2f2f692e696d6775722e636f6d2f31646c7a5157382e706e67"
-			alt="The wildfire minigame" />
+		<figure>
+			<img
+				src="https://camo.githubusercontent.com/999bed0c77f49d2145630fa9476bc91d18912ce0affccb745be612d6681e0ee0/68747470733a2f2f692e696d6775722e636f6d2f31646c7a5157382e706e67"
+				alt="The wildfire minigame" />
+		</figure>
 		<h3 class="h3">Protect the Arctic</h3>
 		<p>
 			The last minigame allows the player to attempt to protect ice in the Arctic from being destroyed. The player controls a paddle and must deflect incoming longwave radiation that destroys the ice.
 			This minigame also becomes more difficult as the planet warms, reflecting the change in polar clouds and cloud cover reflecting more longwave radiation.
 		</p>
-		<img
-			src="https://camo.githubusercontent.com/d7598a2b117a324ad3c15a1b4016b93251c3916c0edf803911193f1cbf359ce6/68747470733a2f2f692e696d6775722e636f6d2f673654584b56712e706e67"
-			alt="The arctic minigame" />
+		<figure>
+			<img
+				src="https://camo.githubusercontent.com/d7598a2b117a324ad3c15a1b4016b93251c3916c0edf803911193f1cbf359ce6/68747470733a2f2f692e696d6775722e636f6d2f673654584b56712e706e67"
+				alt="The arctic minigame" />
+		</figure>
 	</div>
 </section>
