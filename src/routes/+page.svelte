c8327cda7dea8b2ec2a65fb8c9df7ee31d04c8d6
Devi <dacharya64@proton.me>
2024-08-02 12:57:28 -0400

    adding publications page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,8 +9,3 @@
 <section class="homepage">
 	<Landing />
 </section>
-
-<!-- Contact me button -->
-<div class="fixed bottom-0 w-full">
-	<a href="mailto:dacharya64@proton.me" target="blank" type="button" class="btn-icon btn-icon-xl variant-filled float-right contact"><i class="fa-solid fa-envelope"></i></a>
-</div>
---
+++ src/routes/publications-page.svelte/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Header from '../header.svelte';
+	import Publications from '../publications.svelte';
+</script>
+
+<Header />
+<section class="projectpage">
+	<Publications />
+</section>
