c2e0c6990c0bf78d5bdbd6e8b6efb7da212444e1
Devi <dacharya64@proton.me>
2024-08-08 15:59:42 -0400

    changing theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,23 +5,23 @@ import typography from '@tailwindcss/typography';
 import { skeleton } from '@skeletonlabs/tw-plugin';
 
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
-	theme: {
-		extend: {}
-	},
-	plugins: [
-		forms,
-		typography,
-		skeleton({
-			themes: {
-				preset: [
-					{
-						name: 'sahara',
-						enhancements: true
-					}
-				]
-			}
-		})
-	]
+    darkMode: 'class',
+    content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+    theme: {
+        extend: {}
+    },
+    plugins: [
+        forms,
+        typography,
+        skeleton({
+            themes: {
+                preset: [
+                    {
+                        name: 'crimson',
+                        enhancements: true
+                    }
+                ]
+            }
+        })
+    ]
 } satisfies Config;
