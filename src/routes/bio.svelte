1eac78146ed8cacab5a18010302c94b35fc5ac2c
Devi <dacharya64@proton.me>
2024-08-08 10:47:33 -0400

    fixing bio position

--- src/routes/bio.svelte
+++ src/routes/bio.svelte
@@ -2,11 +2,11 @@
 	import Timeline from './timeline.svelte';
 </script>
 
-<div class="grid grid-rows-1 gap-4 grid-cols-[repeat(auto-fit,_14.666666%)] m-auto justify-center">
-	<div class="col-span-1 justify-center justify-self-center mx-auto">
+<div class="grid grid-rows-1 gap-4 lg:grid-cols-[repeat(auto-fit,_14.666666%)] grid-cols-1 m-auto justify-center">
+	<div class="col-start-2 col-span-1 justify-center justify-self-center mx-auto bio-pic">
 		<img class="rounded-full" src="/img/devi.jpg" alt="" />
 	</div>
-	<div class="col-span-5 justify-center justify-self-center mx-auto">
+	<div class="col-span-3 justify-center justify-self-center mx-auto">
 		<div class="bio">
 			<h2 class="h2">
 				<i class="fa-solid fa-gears"></i>
